f2fdc5cc0faf6dbdbb282026db60a165c27c142c
Vinícius Lessa <vinicius.lessa33@outlook.com>
2024-02-13 21:04:48 -0300

    Update CarPlayer.cs

--- rocket-league-remake/Assets/Scripts/CarPlayer.cs
+++ rocket-league-remake/Assets/Scripts/CarPlayer.cs
@@ -7,7 +7,7 @@ public class CarPlayer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World!");
+        Debug.Log("Hello World!!");
     }
 
     // Update is called once per frame
